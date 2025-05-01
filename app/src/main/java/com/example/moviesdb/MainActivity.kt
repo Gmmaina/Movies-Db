e37c4d57fb17170d6d255ff975cfcbea1b674882
Gmmaina <mutondogm@gmail.com>
2025-05-01 07:30:17 +0300

    Add navigation components

--- app/src/main/java/com/example/moviesdb/MainActivity.kt
+++ app/src/main/java/com/example/moviesdb/MainActivity.kt
@@ -1,22 +1,16 @@
 package com.example.moviesdb
 
 import android.os.Bundle
-import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import androidx.recyclerview.widget.LinearLayoutManager
-import com.example.moviesdb.adapter.MoviesAdapter
-import com.example.moviesdb.data.MovieModel
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import com.example.moviesdb.databinding.ActivityMainBinding
-import com.example.moviesdb.network.RetrofitInstance
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
 
 class MainActivity : AppCompatActivity() {
-    private val apiKey = "c89168cb22d9a8b1982f7cb49aabe573"
     private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -29,27 +23,11 @@ class MainActivity : AppCompatActivity() {
             insets
         }
 
-        binding.recyclerView.layoutManager = LinearLayoutManager(this)
-        fetchPopularMovies()
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+
+        binding.bottomNavigation.setupWithNavController(navController)
     }
 
-    private fun fetchPopularMovies() {
-        RetrofitInstance.api.getPopularMovies(apiKey)
-            .enqueue(object : Callback<MovieModel> {
-                override fun onResponse(call: Call<MovieModel>, response: Response<MovieModel>) {
-                    if (response.isSuccessful) {
-                        val movies = response.body()?.results ?: emptyList()
-                        binding.recyclerView.adapter = MoviesAdapter(movies)
-                    } else {
-                        binding.failedTv.text = "Failed to fetch movies"
-                        binding.failedTv.visibility = View.VISIBLE
-                    }
-                }
-
-                override fun onFailure(call: Call<MovieModel>, t: Throwable) {
-                    binding.failedTv.text = "Error fetching movies: ${t.message}"
-                    binding.failedTv.visibility = View.VISIBLE
-                }
-            })
-    }
 }
